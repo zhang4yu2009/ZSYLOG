810aad198114d9457fa4c38b644dba17f2293f62
ZSY <647976802@qq.com>
2017-12-12 17:50:30 +0800

    fixed bug

--- ZSYLOG.podspec
+++ ZSYLOG.podspec
@@ -3,7 +3,7 @@
 Pod::Spec.new do |s|
 
  s.name         = "ZSYLOG"
-    s.version      = "0.0.1"
+    s.version      = "0.0.2"
     s.ios.deployment_target = '7.0'
     s.summary      = "A delightful LOG  framework."
     s.homepage     = "https://github.com/zhang4yu2009/ZSYLOG"
--- ZSYLOG/SYLog.h
+++ ZSYLOG/SYLog.h
@@ -6,7 +6,7 @@
 //  Copyright © 2017年 ZSY. All rights reserved.
 //
 
-#ifdef DEBUG  // 调试状态
+#ifdef DEBUG  // 调试状态// 哈哈
 // 打开LOG功能
 #define SYLog(...) NSLog(__VA_ARGS__)
 #define ZSY(fmt, ...) NSLog((@"[文件名:%s]\n" "[函数名:%s]\n" "[行号:%d] \n" fmt), __FILE__, __FUNCTION__, __LINE__, ##__VA_ARGS__);

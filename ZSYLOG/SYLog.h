34d5fb1e0cd74776abb8f5d62d236592fd86b3e5
ZSY <647976802@qq.com>
2017-12-12 15:47:59 +0800

    first commit for version 0.0.1

---
+++ ZSYLOG/SYLog.h
@@ -0,0 +1,17 @@
+//
+//  SYLog.h
+//  2017-8-7
+//
+//  Created by ZSY on 17/8/14.
+//  Copyright © 2017年 ZSY. All rights reserved.
+//
+
+#ifdef DEBUG  // 调试状态
+// 打开LOG功能
+#define SYLog(...) NSLog(__VA_ARGS__)
+#define ZSY(fmt, ...) NSLog((@"[文件名:%s]\n" "[函数名:%s]\n" "[行号:%d] \n" fmt), __FILE__, __FUNCTION__, __LINE__, ##__VA_ARGS__);
+#else // 发布状态
+// 关闭LOG功能
+#define SYLog(...)
+#define ZSY(...)
+#endif
